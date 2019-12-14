32ce27d78771a7ef616a2ba0ba0e2b30c714177a
priyagund <priyankagund18@gmail.com>
2019-12-14 14:42:32 +0530

    ADDED:writing test case for incorrect file passed

--- src/test/java/com/ipl/IPLMatch2019Test.java
+++ src/test/java/com/ipl/IPLMatch2019Test.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 public class IPLMatch2019Test
 {
   private String IPL_RUNS_RECORD_FILE="/home/admin165/Desktop/Priya/NewIPL2019/src/test/resources/IPL2019FactsheetMostRuns.csv";
+  private String WRONG_FILE_PATH="/home/admin165/Downloads/CensusAnalyser(2)/CensusAnalyser/src/test/resources/USCensusData.csv";
+  private String INCORRECT_HEADER="/home/admin165/Desktop/Priya/NewIPL2019/src/test/resources/IncrrectHeader.csv";
 
   @Test
-    public void givenLoadmatchRunsRecord_ifLoded_shouldReturnResult()
+    public void givenLoadMatchRunsRecord_ifLoded_shouldReturnResult()
     {
         try {
             IPLMatch2019 iplMatch2019=new IPLMatch2019();
@@ -17,8 +19,16 @@ public class IPLMatch2019Test
         } catch (IPLMatchException e) {
             e.printStackTrace();
         }
-
     }
 
-
+    @Test
+    public void givenLoadMatchRunRecord_ifIncorrectFIle_shouldThrowException()
+    {
+        try {
+            IPLMatch2019 iplMatch2019=new IPLMatch2019();
+            iplMatch2019.loadIplPlayersRecord(WRONG_FILE_PATH);
+        } catch (IPLMatchException e) {
+            Assert.assertEquals(IPLMatchException.ExceptionType.SUME_ERROR_IN_FILE,e.type);
+        }
+    }
 }

5f12684fa2aa7e3fd1767d43aff67993ba89f848
priyagund <priyankagund18@gmail.com>
2019-12-14 15:01:13 +0530

    ADDED:writing test cases for in correct header file passed and also test case for incorrect header passed

--- src/test/java/com/ipl/IPLMatch2019Test.java
+++ src/test/java/com/ipl/IPLMatch2019Test.java
@@ -8,9 +8,9 @@ public class IPLMatch2019Test
   private String IPL_RUNS_RECORD_FILE="/home/admin165/Desktop/Priya/NewIPL2019/src/test/resources/IPL2019FactsheetMostRuns.csv";
   private String WRONG_FILE_PATH="/home/admin165/Downloads/CensusAnalyser(2)/CensusAnalyser/src/test/resources/USCensusData.csv";
   private String INCORRECT_HEADER="/home/admin165/Desktop/Priya/NewIPL2019/src/test/resources/IncrrectHeader.csv";
-
+  private String INCORRECT_DELIMETER="/home/admin165/Desktop/Priya/NewIPL2019/src/test/resources/IncorrectDelimiter.csv";
   @Test
-    public void givenLoadMatchRunsRecord_ifLoded_shouldReturnResult()
+    public void givenLoadIPLRunsRecord_ifLoded_shouldReturnResult()
     {
         try {
             IPLMatch2019 iplMatch2019=new IPLMatch2019();
@@ -22,7 +22,7 @@ public class IPLMatch2019Test
     }
 
     @Test
-    public void givenLoadMatchRunRecord_ifIncorrectFIle_shouldThrowException()
+    public void givenIPLRunRecordFile_ifIncorrectFIle_shouldThrowException()
     {
         try {
             IPLMatch2019 iplMatch2019=new IPLMatch2019();
@@ -33,7 +33,7 @@ public class IPLMatch2019Test
     }
 
     @Test
-    public void givenLoadMatchRunRecord_ifEmptyFile_shouldThrowException()
+    public void givenIPLRunRecordFile_ifEmptyFile_shouldThrowException()
     {
         try {
             IPLMatch2019 iplMatch2019=new IPLMatch2019();
@@ -43,4 +43,26 @@ public class IPLMatch2019Test
         }
     }
 
+    @Test
+    public void givenIPLRunRecordFile_ifIncorrctHeader_shouldThrowException()
+    {
+        try {
+            IPLMatch2019 iplMatch2019=new IPLMatch2019();
+            iplMatch2019.loadIplPlayersRecord(INCORRECT_HEADER);
+        } catch (IPLMatchException e) {
+            Assert.assertEquals(IPLMatchException.ExceptionType.SUME_ERROR_IN_FILE,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLRunRecordFile_ifIncorrectDelimeter_shouThrownException()
+    {
+        try {
+        IPLMatch2019 iplMatch2019=new IPLMatch2019();
+        iplMatch2019.loadIplPlayersRecord(INCORRECT_DELIMETER);
+    } catch (IPLMatchException e) {
+        Assert.assertEquals(IPLMatchException.ExceptionType.SUME_ERROR_IN_FILE,e.type);
+    }
+
+    }
 }
